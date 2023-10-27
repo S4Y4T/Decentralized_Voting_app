7c51aa0b4dd26a53482b037e1735c381e1ad8f7b
ethereal147 <125991202+ethereal147@users.noreply.github.com>
2023-10-28 03:15:08 +0600

    Add files via upload

---
+++ Finished.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Finished = (props) => {
+    return (
+        <div className="login-container">
+            <h1 className="welcome-message">Voting is Finished!</h1>
+            <img src="https://media.tenor.com/gzyumPwwZxcAAAAC/this-is-the-end-raining.gif" alt=""></img>
+        </div>
+    )
+}
+
+export default Finished;
